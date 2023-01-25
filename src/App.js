b975bdd1d40c200b118bd2b1cd493f0144081fc2
DorjaSkenderovic <dorjaskenderovic@gmail.com>
2023-01-25 14:18:46 +0100

    finished app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import styles from './app.module.scss';
 import React from 'react';
+import Cookies from 'js-cookie'
 
 function App() {
+  const todosCookie = 'todosArray'
   const inputRef = React.useRef(null);
-  const [todos, setTodos] = React.useState([]);
+  const todosArray = (Cookies.get(todosCookie)) ? Cookies.get(todosCookie).split(',') : []
+  const [todos, setTodos] = React.useState(todosArray);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,8 +18,9 @@ function App() {
       }
     }
     if (curr.trim() && inputVal) {
-
+      Cookies.set(todosCookie, [...todos, curr],{ expires: 1})
       setTodos(prevTodos => [...prevTodos, curr]);
+
 
     } else if (inputVal === false) {
       alert("Already in list!")
@@ -27,6 +31,7 @@ function App() {
   };
 
   const handleDelete = (todo) => {
+    Cookies.set(todosCookie, todos.filter(e => e !== todo))
     setTodos(prevTodos => prevTodos.filter(e => e !== todo))
   }
 
@@ -60,7 +65,7 @@ function App() {
             placeholder="Add your new todo"
           />
           <button type='submit'>
-            <svg className={styles.addIcon} width="36px" height="36px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg className={styles.addIcon} width="36px" height="36px" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
               <g id="style=linear">
                 <g id="add-box">
                   <path id="vector" d="M2 8C2 4.68629 4.68629 2 8 2H16C19.3137 2 22 4.68629 22 8V16C22 19.3137 19.3137 22 16 22H8C4.68629 22 2 19.3137 2 16V8Z" stroke="#1a1e24" strokeWidth="2" />
